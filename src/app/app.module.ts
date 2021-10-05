589e6e2de36ee57a49aa701042b888be56f9bcd4
U6064297 <sai.ura999@gmail.com>
2021-10-05 15:46:58 +0530

    added pagination& splitted into smaller components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { PaginatorModule } from 'primeng/paginator';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { JwPaginationModule } from 'jw-angular-pagination';
 
 
 @NgModule({
-  declarations: [AppComponent, EmployeeDetailsComponent],
+  declarations: [
+    AppComponent,
+    EmployeeDetailsComponent,
+    AddEmployeeComponent,
+    UpdateEmployeeComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule,
     ReactiveFormsModule,
     HttpClientModule,
     FormsModule,
-  
+    JwPaginationModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { ApiService } from '../Shared/api.service';
-import { Employee } from './employee';
 import { EmployeeInterface } from './employeeInterface';
 
 @Component({
@@ -10,15 +8,14 @@ import { EmployeeInterface } from './employeeInterface';
   styleUrls: ['./employee-details.component.css'],
 })
 export class EmployeeDetailsComponent implements OnInit {
+
   employees: EmployeeInterface[] = [];
-  addBtn!: boolean;
-  updateBtn!: boolean;
-  formValue!: FormGroup;
-  clickedId: number = 0;
-  modal_title: string = 'Add Employee';
+
   private _listFilter: string = '';
 
   filteredEmployees: EmployeeInterface[] = this.employees;
+
+  pageOfItems: EmployeeInterface[] = [];
 
   get listFilter(): string {
     return this._listFilter;
@@ -29,108 +26,36 @@ export class EmployeeDetailsComponent implements OnInit {
     console.log('In setter: ', value);
     this.filteredEmployees = this.performFilter(value);
   }
-  setListFilter(value: string) {
-    this._listFilter = value;
-    console.log('Setting: ', value);
-    this.filteredEmployees = this.performFilter(value);
-  }
 
-  employee: Employee = new Employee();
-
-  constructor(private formBuilder: FormBuilder, private api: ApiService) {}
+  constructor(private api: ApiService) {}
 
   ngOnInit(): void {
-    this.formValue = this.formBuilder.group({
-      name: [''],
-      salary: [''],
-      age: [''],
-      profilePic: [''],
-    });
-
+    console.log('on init');
     this.getAllEmployees();
   }
 
-  postEmployee() {
-    
-    this.employee.employee_name = this.formValue.value.name;
-    this.employee.employee_age = this.formValue.value.age;
-    this.employee.employee_salary = this.formValue.value.salary;
-    this.employee.profile_image =
-      '/assets/' +
-      (this.formValue.value.profilePic
-        ? this.formValue.value.profilePic.split('\\')[2]
-        : 'profile.png');
-        console.log(this.formValue.controls.salary.value);
-
-    this.api.postEmployee(this.employee).subscribe(
-      (res) => {
-        console.log(res);
-        alert('added');
-        this._listFilter = '';
-        this.formValue.reset();
-        this.getAllEmployees();
-        document.getElementById('back')!.click();
-      },
-      (err) => {
-        alert('wrong');
-      }
-    );
-  }
-
   getAllEmployees() {
-    this.api.getEmployee().subscribe((res) => {
-      this.employees = res.data;
-      this.filteredEmployees = this.employees;
-    });
-  }
-
-  EditEmployee(Employee: EmployeeInterface) {
-    this.modal_title = 'Update Employee';
-    this.addBtn = false;
-    this.updateBtn = true;
-    this.clickedId = Employee.id;
-    this.formValue.controls['name'].setValue(Employee.employee_name);
-    this.formValue.controls['age'].setValue(Employee.employee_age);
-    this.formValue.controls['salary'].setValue(Employee.employee_salary);
-    this.employee.profile_image = Employee.profile_image;
-  }
-
-  UpdateEmployee() {
-    this.employee.employee_name = this.formValue.value.name;
-    this.employee.employee_age = this.formValue.value.age;
-    this.employee.employee_salary = this.formValue.value.salary;
-    this.employee.profile_image = this.formValue.value.profilePic
-      ? '/assets/' + this.formValue.value.profilePic.split('\\')[2]
-      : this.employee.profile_image;
-
-    this.api.updateEmployee(this.employee, this.clickedId).subscribe(
+    this.api.getEmployee().subscribe(
       (res) => {
-        console.log(res);
-        alert('Updated');
-        this.formValue.reset();
-        this.getAllEmployees();
-        this.setListFilter(this._listFilter);
-        document.getElementById('back')!.click();
+        this.employees = res.data;
+        this.filteredEmployees = this.employees;
       },
       (err) => {
-        alert('wrong');
+        alert('Unable to fetch data from server');
       }
     );
   }
+
   performFilter(value: string): EmployeeInterface[] {
     value = value.toLowerCase();
     return this.employees.filter((employee: EmployeeInterface) =>
       employee.employee_name.toLowerCase().includes(value)
     );
   }
-  addClicked() {
-    this.modal_title = 'Add Employee';
-    this.addBtn = true;
-    this.updateBtn = false;
-    this.formValue.reset();
+
+  onChangePage(pageOfItems: EmployeeInterface[]) {
+    // update current page of items
+    this.pageOfItems = pageOfItems;
   }
 
-  paginate(event: any) {
-    console.log(event);
-  }
 }
